24aef9dcc37dc5b3c669999ff826210732a69ffb
Ana Luiza Guimaraes <analu@MacBook-Air-de-Ana.local>
2021-04-21 13:51:37 -0300

    first commit

---
+++ exe-1/src/Main.java
@@ -0,0 +1,46 @@
+package exercicio_1;
+
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290; 
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50; 
+		double measure = 53.234567;
+		
+		// MANEIRA 1
+		
+		System.out.printf("Products:%n%s, which price is $ %.2f%n %s, which price is $ %.2f%n%n Record: %d years old, code %d and gender: %s%n%n Measure with eight decimal places: %.8f%n Rouded (three decimal places): %.3f%n", product1, price1, product2, price2, age, code, gender, measure, measure);
+				
+		// MANEIRA 2 - PREFERIDA
+		
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
+		System.out.printf("Record: %d years old, code %d and gender: %s%n%n", age, code, gender);
+		System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n%n", measure);
+	
+		// MANEIRA 3 
+		
+		System.out.println("Products:");
+		System.out.println(product1 + ", which price is $ " + price1);
+		System.out.println(product2 + ", which price is $ " + price2);
+		System.out.println("Record: " + age + " years old, code " + code + " and gender: " + gender);
+
+	}
+
+}
